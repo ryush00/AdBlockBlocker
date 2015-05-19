3a56402d95f2becf507ea6be71667dbae43a4080
SeongHoon Ryu <ryush00@gmail.com>
2015-05-20 00:02:10 +0900

    Update adblockblock.js

--- adblockblock.js
+++ adblockblock.js
@@ -1,7 +1,23 @@
+
+function passImageTest() {
+    console.log("PASS");
+    isAdBlockActive = false;
+    checkAgain();
+}
+
+function notPassImageTest() {
+    console.log("NOT PASS");
+    isAdBlockActive = true;
+    checkAgain();
+}
+    
+
     function adBlockDetected() {
         var element = document.getElementById("article");
         element.innerHTML = '<img src="./images/adblock-detected.png" /><a href="http://info.flagcounter.com/MQQT"><img src="http://s09.flagcounter.com/count/MQQT/bg_FFFFFF/txt_000000/border_CCCCCC/columns_2/maxflags_6/viewers_0/labels_0/pageviews_1/flags_0/" alt="Flag Counter" border="0"></a>';
     }
+
+    
     function adBlockNotDetected() {
     
     }
@@ -13,27 +29,34 @@
     }
 
     function checkAgain() {
-        try {
             fuckAdBlock.onDetected(adBlockDetected).onNotDetected(adBlockNotDetected);
             setTimeout(function() {
             fuckAdBlock.check();
             }, 500);
             
             
-            if($(".adsbygoogle").contents()[0] == null) {
-                isAdBlockActive = true
+            try {
+                if($(".adsbygoogle").contents()[0] == null) {
+                    isAdBlockActive = true
+                }
+            } catch (err) {
+                console.log(err);
             }
+            
+            try {
+                if($('.adsbygoogle').attr('data-adsbygoogle-status') != "done") {
+                    //isAdBlockActive = true data-adsbygoogle-status 기반 체크 추가해야함
+                }
+            } catch (err) {
+                console.log(err);
+            }
+            
             if (isAdBlockActive) { 
               console.log("The visitor is blocking ads");
               adBlockDetected()
             }
-        } catch (err) {
-        
-        }
             setTimeout(function() {
                 checkAgain();
             }, 500);
         
     }
-
-$( checkAgain() );
