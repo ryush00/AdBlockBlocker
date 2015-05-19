043a7e4a0a78255cbb3b22eaf9b4bf5dd95626fe
SeongHoon Ryu <ryush00@gmail.com>
2015-05-19 23:01:08 +0900

    Update adblockblock.js

--- adblockblock.js
+++ adblockblock.js
@@ -1,5 +1,6 @@
     function adBlockDetected() {
-      #Something action here
+        var element = document.getElementById("article");
+        element.innerHTML = '<img src="./images/adblock-detected.png" /><a href="http://info.flagcounter.com/MQQT"><img src="http://s09.flagcounter.com/count/MQQT/bg_FFFFFF/txt_000000/border_CCCCCC/columns_2/maxflags_6/viewers_0/labels_0/pageviews_1/flags_0/" alt="Flag Counter" border="0"></a>';
     }
     function adBlockNotDetected() {
     
